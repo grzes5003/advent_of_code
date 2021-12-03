91044531f227cf6e235bad5bf460780f976772a5
grzes5003 <grzes.kulinski@gmail.com>
2021-12-03 20:54:44 +0100

    Solution to day03

--- src/day03.rs
+++ src/day03.rs
@@ -1,3 +1,6 @@
+use std::ops::BitXor;
+use std::os::raw::c_int;
+
 fn task01(input: Vec<String>) -> u32 {
     let len = (input.len() / 2) as u32;
 
@@ -27,6 +30,46 @@ fn task01(input: Vec<String>) -> u32 {
             .unwrap()) as u32
 }
 
+
+fn _task02(input: Vec<Vec<u8>>, idx: usize, version: bool) -> Vec<String> {
+    let len = (input.len() as f32 )/ 2 as f32;
+    if input.len() == 1 {
+        return input.get(0).unwrap().into_iter().map(|val| val.to_string()).collect();
+    }
+
+    let find_more = match input.iter()
+        .map(|val| {
+            *val.get(idx).unwrap() as u16
+        }
+        )
+        .reduce(|accum, item| accum + item).unwrap() as f32{
+        val if val >= len => version as u8,
+        val if val < len => !version as u8,
+        _ => panic!("")
+    };
+
+    _task02(input.into_iter()
+                .filter(|vec| *vec.get(idx).unwrap() != find_more)
+                .collect(),
+            idx + 1,
+            version,
+    )
+}
+
+fn task02(input: Vec<String>) -> u32 {
+    let v_input = input.iter()
+        .map(|val| val.chars().map(|ch| ch.to_digit(10).unwrap() as u8)
+            .collect::<Vec<u8>>()).collect::<Vec<Vec<u8>>>();
+
+    let val1 = _task02(v_input.clone(), 0, true).join("");
+
+    let val2 = _task02(v_input, 0, false).join("");
+
+    (isize::from_str_radix(val1.as_str(), 2).unwrap()
+        * isize::from_str_radix(val2.as_str(), 2).unwrap()) as u32
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -41,5 +84,32 @@ mod tests {
         let vec = input_data();
         let result = task01(vec);
         println!("task01: {}", result)
+    }
+
+    #[test]
+    fn task02_example_test() {
+        let vec = vec![
+            String::from("00100"),
+            String::from("11110"),
+            String::from("10110"),
+            String::from("10111"),
+            String::from("10101"),
+            String::from("01111"),
+            String::from("00111"),
+            String::from("11100"),
+            String::from("10000"),
+            String::from("11001"),
+            String::from("00010"),
+            String::from("01010"),
+        ];
+        let result = task02(vec);
+        println!("task02_example: {}", result)
+    }
+
+    #[test]
+    fn task02_test() {
+        let vec = input_data();
+        let result = task02(vec);
+        println!("task02: {}", result)
     }
 }
